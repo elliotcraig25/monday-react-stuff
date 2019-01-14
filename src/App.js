3807a5baa6b90057cbd93faa34d64a125a424589
elliot <elliotgcraig@icloud.com>
2019-01-14 11:22:54 -0700

    Color box added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Components/Header'
+import Color from './Components/Color'
 
 class App extends Component {
   constructor(){
@@ -8,16 +9,27 @@ class App extends Component {
     this.state = {
       userName: null,
     }
+    // this.handleNameUpdate = this.handleNameUpdate.bind(this)
   }
+
+  handleNameUpdate(value){
+    console.log('update name ran')
+    this.setState({
+      userName: value
+    })
+  }
+
   render() {
     return (
       <div className='App'>
-        <input />
+        <input onChange={e=>this.handleNameUpdate(e.target.value)}/>
         {this.state.userName
-        ? <Header userName={this.state.userName}/>
+        ? <div>
+            <Header userName={this.state.userName}/>
+            <Color />
+          </div>
         : 'Please Enter User Name'
         }
-        
       </div>
     );
   }
---
+++ src/Components/Color.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Color extends Component {
+    render(){
+        return (
+            <div style={{height: '75px', width: '75px', backgroundColor: 'tomato'}}>
+                
+            </div>
+        )
+    }
+}
+
+export default Color
